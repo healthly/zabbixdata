00a12484313220dbbdb2342fe474241fcdedeb29
vm5 <healthly@qq.com>
2013-10-17 16:56:58 +0800

    fix find trends data from zabbix api 2.0

--- trendsZBX.py
+++ trendsZBX.py
@@ -34,20 +34,20 @@ def zapi():
 	
 def gHostid():
 	hostid1 = {}
-	for h in zapi().host.get(output='extend'):
+	for h in zapi().host.get(groupids=['8','11','12','6','15'],output='extend'):
 		i = h['hostid']
 		j = h['name']
 		hostid1[i] = j
 		
 	return hostid1
 
-def gItemid(HostidL,temkey):
+def gItemid(HostidL,itemkey):
 	item1 = {}
-	for Hostid in HostidL:
-		for h in zapi().item.get(hostids=Hostid,search={"key_":itemkey}):
+	for id in HostidL:
+		for h in zapi().item.get(hostids=id,search={"key_":itemkey},output='extend'):
 			i = h['itemid']
 			j = h['key_']
-			item1[i] = [HostidL[Hostid],j]
+			item1[i] = [HostidL[id],j]
 	
 	return item1
 
@@ -62,47 +62,76 @@ def time1():
 	return timeA
 
 
-def gTrends(itemlist,timeS):
+def gTrends(itemlist):
 	value1 = []
 	valuedays = []
 	valueavg = {}
-	valueall = {}
 	for i in itemlist:
 		if len(valuedays):
 			del valuedays[:]
-			
-		for j in timeS:
-			if len(value1):
-				del value1[:]
-			
-			# Create a time range
-			item_id = i
-			time_till = j
-			time_from = time_till - 60 * 60 * 14 # 14 hours
+		if len(value1):
+			del value1[:]
+		item_id = i
+		time_till = time.mktime(datetime.now().timetuple())
+		time_from = time_till - 60 * 60 * 24 * 90
+		history = zapi().trends.get(itemids=[item_id],
+			time_from=time_from,
+			time_till=time_till,
+			output='extend',
+			#limit='2000',
+		)
+		for point in history:
+			value1.append(int(point['value_avg']))
+		if len(value1):
+			max1 = max(value1)
+			valueavg[max1] = [i,itemlist[i][0],itemlist[i][1]]
+		else:
+			continue
 		
-			# Query item's trend data
-			history = zapi().trends.get(itemids=[item_id],
-				time_from=time_from,
-				time_till=time_till,
-				output='extend',
-				#limit='2000',
-			)
-		
-			for point in history:
-				value1.append(int(point['value_avg']))
-			avg1 = sum(value1) / 14
-			valuedays.append(avg1)
-		avg = sum(valuedays)/len(valuedays)
-		valueavg[(i,itemlist[i][0]),itemlist[i][1])] = avg
-		
-	#return valueavg
-	print valueavg
+	return valueavg
+
 	
+def gTrends1(itemlist):
+	value1 = []
+	valuedays = []
+	valueavg = {}
+	for i in itemlist:
+		if len(valuedays):
+			del valuedays[:]
+		if len(value1):
+			del value1[:]
+		item_id = i
+		time_till = time.mktime(datetime.now().timetuple())
+		time_from = time_till - 60 * 60 * 24 * 120 # 120 days
+		history = zapi().trends.get(itemids=[item_id],
+			time_from=time_from,
+			time_till=time_till,
+			output='extend',
+			#limit='5000',
+			history=0,
+			)
+		for point in history:
+			value1.append(float(point['value_avg']))
+		if len(value1):
+			max1 = max(value1)
+			valueavg[max1] = [i,itemlist[i][0],itemlist[i][1]]
+		else:
+			continue
+		
+	return valueavg
+
 if __name__=="__main__":
-	#h = gHostid()
-	h = {u'10141':'nginx111'}
-	t = time1()
-	for a in h:
-		k = u"net.if"
-		l = gItemid(a,k)
-		gTrends(l,t)
+	h = gHostid()
+	k = u"net.if"
+	l = gItemid(h,k)
+	k2 = u"system.cpu.load[all,avg5]"
+	l2 = gItemid(h,k2)
+	trends = gTrends(l)
+	trends2 = gTrends1(l2)
+	for i in trends:
+		v = float(i)/1024/1024
+		v = float('%0.3f' % v)
+		print "host: %s,item-net: %s,netflow: %sMps" % (trends[i][1],trends[i][2],v)
+	print '#####################################################################################################'
+	for j in trends2:
+		print "host: %s,item-load: system.cpu.loadavg5,cpuload: %s" % (trends2[j][1],j)
